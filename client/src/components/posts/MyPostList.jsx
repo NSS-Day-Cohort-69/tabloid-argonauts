c8207cfb06193663829554b87aefbdedc54c6c93
/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/stevebrownlee/b146bf49071c46c41eddf5778b147a71/raw/a1f8f3bebf7fab75e5bfb0d1ecabc89ab22baec8/verify-installs.sh) <christinam1215@yahoo.com>
2024-05-30 10:41:45 -0500

    adding tag to a post

--- client/src/components/posts/MyPostList.jsx
+++ client/src/components/posts/MyPostList.jsx
@@ -1,56 +1,63 @@
 import { useState, useEffect } from "react";
 import { getPosts } from "../../managers/postManager";
-import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from "reactstrap";
-
-
+import {
+  Button,
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
 const MyPostList = ({ loggedInUser }) => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const postsData = await getPosts();
-                const userPosts = postsData
-                    .filter(post => post.userProfileId === loggedInUser.id)
-                    .sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
-                setPosts(userPosts);
-            } catch (error) {
-                console.error("Error fetching posts:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const postsData = await getPosts();
+        const userPosts = postsData
+          .filter((post) => post.userProfileId === loggedInUser.id)
+          .sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
+        setPosts(userPosts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
 
-        fetchData();
-    }, [loggedInUser]);
+    fetchData();
+  }, [loggedInUser]);
 
-    return (
-        <>
-            <h1>Posts List</h1>
-            {posts.map((p) => (
-                <Card
-                    key={p.id}
-                    style={{
-                        width: "10rem",
-                    }}
-                >
-                    <CardBody>
-                        <CardTitle tag="h5">{p.title}</CardTitle>
-                        <CardSubtitle className="mb-2 text-muted" tag="h6">
-                            {p.userProfile?.fullName}
-                        </CardSubtitle>
-                        <CardText>{p.category?.categoryName}</CardText>
-                        <Button
-                            onClick={() => {
-                                navigate(`/api/posts/${p.id}`);
-                            }}
-                        >
-                            View Post
-                        </Button>
-                    </CardBody>
-                </Card>
-            ))}
-        </>
-    );
+  return (
+    <>
+      <h1>Posts List</h1>
+      {posts.map((p) => (
+        <Card
+          key={p.id}
+          style={{
+            width: "10rem",
+          }}
+        >
+          <CardBody>
+            <CardTitle tag="h5">{p.title}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              {p.userProfile?.fullName}
+            </CardSubtitle>
+            <CardText>{p.category?.categoryName}</CardText>
+            <Button
+              onClick={() => {
+                navigate(`/posts/${p.id}`);
+              }}
+            >
+              View Post
+            </Button>
+          </CardBody>
+        </Card>
+      ))}
+    </>
+  );
 };
 
 export default MyPostList;
--- client/src/components/posts/PostDetails.jsx
+++ client/src/components/posts/PostDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getPostById } from "../../managers/postManager";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import {
   Card,
   CardBody,
@@ -8,14 +8,33 @@ import {
   CardSubtitle,
   CardText,
   CardFooter,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
+import { Link } from "react-router-dom";
+import { GetAllTags } from "../../managers/TagManager";
 
-export const PostDetails = () => {
+export const PostDetails = ({ loggedInUser }) => {
   const [post, setPost] = useState({});
+  const [modal, setModal] = useState(false);
+  const [tags, setTags] = useState([]);
+  const [tagSelections, setTagSelections] = useState([]);
   const { id } = useParams();
+  const toggle = () => setModal(!modal);
 
   useEffect(() => {
     getPostById(id).then((obj) => setPost(obj));
+  }, [id]);
+
+  useEffect(() => {
+    GetAllTags().then((arr) => setTags(arr));
   }, []);
 
   const formatDate = (dateString) => {
@@ -25,11 +44,19 @@ export const PostDetails = () => {
     return new Intl.DateTimeFormat("en-US", options).format(date);
   };
 
+  const handleInputChange = (event) => {
+    const tag = parseInt(event.target.id);
+    if (event.target.checked) {
+      setTagSelections((prevSelections) => [...prevSelections]);
+    }
+  };
+
   return (
     <>
       <Card
+        key={id}
         style={{
-          width: "18rem",
+          width: "25rem",
         }}
       >
         <img alt="Sample" src={post.headerImage} />
@@ -38,13 +65,38 @@ export const PostDetails = () => {
           <CardSubtitle className="mb-2 text-muted" tag="h6">
             {post.userProfile?.userName}
           </CardSubtitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6">
-            <Link to ={`/posts/${post.id}/comments`}>View Comments</Link>
+          <CardSubtitle>
+            <Link to={`/posts/${post.id}/comments`}>View Comments</Link>
           </CardSubtitle>
           <CardText>{post.content}</CardText>
+          {post?.userProfile?.Id === loggedInUser?.Id ? (
+            <Button onClick={toggle}>Manage Tags</Button>
+          ) : (
+            ""
+          )}
         </CardBody>
         <CardFooter>{formatDate(post.publicationDate)}</CardFooter>
       </Card>
+
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Choose tags for post</ModalHeader>
+        <ModalBody>
+          <Form onSubmit>
+            {tags.map((t) => (
+              <FormGroup check key={t.id}>
+                <Input id={t.id} type="checkbox" value={t.name} onChange />{" "}
+                <Label check>{t.tagName}</Label>
+              </FormGroup>
+            ))}
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Save</Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
