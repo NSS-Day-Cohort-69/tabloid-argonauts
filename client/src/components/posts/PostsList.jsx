e659094fdd83d8d275d256b2cf62173758c95b03
/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/stevebrownlee/b146bf49071c46c41eddf5778b147a71/raw/a1f8f3bebf7fab75e5bfb0d1ecabc89ab22baec8/verify-installs.sh) <christinam1215@yahoo.com>
2024-05-31 14:22:41 -0500

    added filters to all opsts

--- client/src/components/posts/PostsList.jsx
+++ client/src/components/posts/PostsList.jsx
@@ -7,18 +7,98 @@ import {
   CardSubtitle,
   CardText,
   Button,
+  FormGroup,
+  Label,
+  Input,
+  Form,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
 } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import { getAllCategories } from "../../managers/categoryManager";
+import "./posts.css";
 
 export const PostsList = () => {
   const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [categorySearch, setCategorySearch] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     getPosts().then((arr) => setPosts(arr));
+  }, [searchTerm]);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  const clearSearchFilters = () => {
+    getPosts().then((arr) => setPosts(arr));
+  };
+
+  useEffect(() => {
+    getAllCategories().then(setCategories);
   }, []);
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    getPosts(searchTerm).then(setPosts);
+  };
+
+  const handleCategoryInputChange = (event) => {
+    const selectedCategoryId = event.currentTarget.getAttribute("value");
+    setCategorySearch(selectedCategoryId);
+    getPosts("", parseInt(selectedCategoryId)).then(setPosts);
+  };
   return (
     <>
+      <div>
+        <Form inline onSubmit={handleSearchSubmit}>
+          <FormGroup>
+            <Label for="search" hidden>
+              Search
+            </Label>
+            <Input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search by tag"
+              value={searchTerm}
+              onChange={handleSearchInputChange}
+            />
+          </FormGroup>
+          <Button type="submit">Search</Button>
+        </Form>
+      </div>
+      <div className="search-controls">
+        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle caret>Sort By Category</DropdownToggle>
+          <DropdownMenu>
+            {categories.map((c) => (
+              <DropdownItem
+                key={c.id}
+                value={c.id}
+                onClick={handleCategoryInputChange}
+              >
+                {c.categoryName}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+        <Button
+          onClick={() => {
+            clearSearchFilters();
+          }}
+        >
+          Clear filters
+        </Button>
+      </div>
       {posts.map((p) => (
         <Card
           key={p.id}
